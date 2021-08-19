724b4798542d08d9e39f6ab30c0d89bcdc927885
Sijan Shrestha <dsijan95@gmail.com>
2021-08-19 12:20:27 +0545

    API base url update

--- component/utils/const.js
+++ component/utils/const.js
@@ -1,4 +1,4 @@
 export const token_key = "token";
 //export const base_URL = "http://192.168.31.218:8000";
-export const base_URL = "http://13.51.64.20:8000";
+export const base_URL = "http://13.51.6.243:8000";
 //export const base_URL = "http://192.168.1.107:8000";
